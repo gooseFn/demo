ef8be3b2a8a41cb5ae7d8ae55dc55976599e1b86
gooseFn <oleg13052006@gmail.com>
2026-03-03 01:21:36 +0500

    Применил patch

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,28 +1,26 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PostService {
 
-    private List<Post> posts;
-    {
-        posts = new ArrayList<>();
-        posts.add(new Post(0L,"Первый пост:)", new Date()));
-        posts.add(new Post(1L,"Второй пост:(", new Date()));
-        posts.add(new Post(2L,"Третий пост:|", new Date()));
-    }
+    @Autowired
+    PostRepository postRepository;
+
     public List<Post> listAllPosts() {
-        return posts;
+        return StreamSupport.stream(postRepository.findAll().spliterator(), false).toList();
     }
 
     public void create(String text) {
-        Long newId = (long) (posts.size() + 1);
-        posts.add(new Post(newId, text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
