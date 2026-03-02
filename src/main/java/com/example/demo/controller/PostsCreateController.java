c5dfba2e1f82bf90dd5aef4c100b7593d5e02dd6
gooseFn <oleg13052006@gmail.com>
2026-03-02 22:16:24 +0500

    Сделал создание нового поста

---
+++ src/main/java/com/example/demo/controller/PostsCreateController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PostsCreateController {
+
+    @Autowired
+    PostService postService;
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String create() {
+        return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        //System.out.println("Текст поста : " + text);
+        postService.create(text);
+        return "redirect:/";
+    }
+}
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,17 +3,25 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
 
+    private List<Post> posts;
+    {
+        posts = new ArrayList<>();
+        posts.add(new Post("Первый пост:)", new Date()));
+        posts.add(new Post("Второй пост:(", new Date()));
+        posts.add(new Post("Третий пост:|", new Date()));
+    }
     public List<Post> listAllPosts() {
-        return List.of(
-            new Post("Пост номер 1", new Date()),
-            new Post("Пост номер 2", new Date()),
-            new Post("Пост номер 3", new Date())
-        );
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
